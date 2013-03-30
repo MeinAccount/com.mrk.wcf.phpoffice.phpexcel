9223f7c696783872d94a36bb5eac77b4c5bd2ca0
Magnus Kühn <magnus.khn@gmail.com>
2013-03-30 17:25:19 +0100

    Adding open and save wrappers

--- files/lib/system/phpoffice/PHPExcel.class.php
+++ files/lib/system/phpoffice/PHPExcel.class.php
@@ -1,5 +1,6 @@
 <?php
 namespace wcf\system\phpoffice;
+use wcf\system\exception\SystemException;
 use wcf\system\SingletonFactory;
 
 /**
@@ -16,5 +17,45 @@ class PHPExcel extends SingletonFactory {
 	 */
 	protected function init() {
 		require_once(WCF_DIR.'lib/system/api/phpoffice/phpexcel/Classes/PHPExcel/IOFactory.php');
+	}
+	
+	/**
+	 * Opens a file in PHPExcel
+	 * 
+	 * @param	string	$filename
+	 * @return	PHPExcel
+	 */
+	public function open($filename) {
+		try {
+			return \PHPExcel_IOFactory::load($filename);
+		} catch (\PHPExcel_Exception $exception) {
+			$this->handleException($exception);
+		}
+	}
+	
+	/**
+	 * Saves a file
+	 * 
+	 * @param	\PHPExcel	$file
+	 * @param	string		$filename
+	 * @param	string		$writerType
+	 */
+	public function save(\PHPExcel $file, $filename, $writerType = 'Excel2007') {
+		try {
+			$writer = \PHPExcel_IOFactory::createWriter($file, $writerType);
+			$writer->save($filename);
+		} catch (\PHPExcel_Exception $exception) {
+			$this->handleException($exception);
+		}
+	}
+	
+	/**
+	 * Handles an exception from PHPExcel
+	 * 
+	 * @param	\PHPExcel_Exception	$exception
+	 * @throws	wcf\system\exception\SystemException
+	 */
+	protected function handleException(\PHPExcel_Exception $exception) {
+		throw new SystemException($exception->getMessage(), $exception->getCode(), '', $exception);
 	}
 }
